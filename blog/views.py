30b860161fac4e2092c514fea0738f4646b29f39
shlee2792 <shlee2792@naver.com>
2019-07-18 16:04:25 +0900

    tag page tested

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post,Category
+from .models import Post,Category, Tag
 from django.views.generic import ListView, DetailView
 
 # def index(request):
@@ -29,6 +29,23 @@ class PostDetail(DetailView):
 
         return context
 
+class PostListByTag(ListView):
+    def get_queryset(self):
+        tag_slug = self.kwargs['slug']
+        tag = Tag.objects.get(slug = tag_slug)
+
+        return tag.post_set.order_by('-created')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        tag_slug = self.kwargs['slug']
+        context = super(type(self), self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+        context['tag'] = Tag.objects.get(slug = tag_slug)
+
+        return context
+
+
 class PostListByCategory(ListView):
 
     def get_queryset(self):
@@ -54,6 +71,9 @@ class PostListByCategory(ListView):
             context['category'] = category
         # context['title'] = 'Blog - {}'.format(category.name)
         return context
+
+
+
 
 
 
